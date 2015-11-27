google/vim-searchindex

commit 5c390af1ff65fb0c1a585e25e93c37782d8630be

If user has mapped 'g/', don't overwrite it. See #6.

plugin/searchindex.vim
@@ -43,7 +43,8 @@ endif
 command! -bar SearchIndex call <SID>PrintMatches()
 
 if !hasmapto('<Plug>SearchIndex', 'n')
-  nmap <silent> g/ <Plug>SearchIndex
+  " If user has mapped 'g/', don't override it.
+  silent! nmap <silent> <unique> g/ <Plug>SearchIndex
 endif
 
 noremap  <Plug>SearchIndex <Nop>
@@ -59,7 +60,7 @@ map <expr> #  <SID>StarSearch('#')
 map <expr> g* <SID>StarSearch('g*')
 map <expr> g# <SID>StarSearch('g#')
 
-" Remap searches from '/' and 'g/' by plugging into <CR> in cmdline & cmdwin.
+" Remap searches from '/' and 'q/' by plugging into <CR> in cmdline & cmdwin.
 cmap <silent> <expr> <CR> <SID>handle_cr()
 function! s:handle_cr()
   if getcmdtype() =~ '[/?]'

commit ef5f7984e4086ee970ecb2aeb044340a523744f4

Remove flickering found in #8.
This bug was introduced by 5c390af. Apparently, n/N mappings still need
<silent> to suppress normal search output.

plugin/searchindex.vim
@@ -52,8 +52,8 @@ noremap! <Plug>SearchIndex <Nop>
 nnoremap <silent> <Plug>SearchIndex :call <SID>PrintMatches()<CR>
 
 " Remap search commands.
-nmap n  n<Plug>SearchIndex
-nmap N  N<Plug>SearchIndex
+nmap <silent> n n<Plug>SearchIndex
+nmap <silent> N N<Plug>SearchIndex
 
 map  *  <Plug>ImprovedStar_*<Plug>SearchIndex
 map  #  <Plug>ImprovedStar_#<Plug>SearchIndex
@@ -66,6 +66,8 @@ noremap <silent> <expr> <Plug>ImprovedStar_g* <SID>StarSearch('g*')
 noremap <silent> <expr> <Plug>ImprovedStar_g# <SID>StarSearch('g#')
 
 " Remap searches from '/' and 'q/' by plugging into <CR> in cmdline & cmdwin.
+" NOTE: This cannot use <silent> - it would break cmdline refresh in some
+" cases (e.g. multiline commands, <C-R>= usage).
 cmap <expr> <CR> <SID>handle_cr()
 function! s:handle_cr()
   if getcmdtype() =~ '[/?]'